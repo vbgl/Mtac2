dd1b6a30423f90ce3cc2a1792eb628c44077e851
Jan-Oliver Kaiser <janno@mpi-sws.org>
2018-03-20 16:31:34 +0100

    Avoid constr. `meq` terms w/ CPS `unify` primitive.

--- src/mConstr.ml
+++ src/mConstr.ml
@@ -56,7 +56,7 @@ type 'a mconstr_head =
   | Mdestcase : (arg_type * arg_any) mconstr_head
   | Mconstrs : (arg_type * arg_any) mconstr_head
   | Mmakecase : (arg_case) mconstr_head
-  | Munify : (arg_type * arg_any * arg_any * arg_any) mconstr_head
+  | Munify : (arg_type * arg_type * arg_any * arg_any * arg_any * arg_fun * arg_fun) mconstr_head
   | Munify_univ : (arg_type * arg_type * arg_any) mconstr_head
   | Mget_reference : (arg_string) mconstr_head
   | Mget_var : (arg_string) mconstr_head
@@ -115,7 +115,7 @@ let num_args_of_mconstr (type a) (mh : a mconstr_head) =
   | Mdestcase -> 2
   | Mconstrs -> 2
   | Mmakecase -> 1
-  | Munify -> 4
+  | Munify -> 7
   | Munify_univ -> 3
   | Mget_reference -> 1
   | Mget_var -> 1
@@ -230,7 +230,7 @@ let isconstrs  = isconstr mkconstrs
 let mkmakecase = mkconstr "makecase"
 let ismakecase  = isconstr mkmakecase
 
-let mkunify = mkconstr "unify"
+let mkunify = mkconstr "unify_cnt"
 let isunify  = isconstr mkunify
 
 let mkunify_univ = mkconstr "unify_univ"
@@ -509,7 +509,7 @@ let mconstr_of (type a) args (h : a mconstr_head) =
   | Mmakecase ->
       MConstr (Mmakecase, (args 0))
   | Munify ->
-      MConstr (Munify, (args 0, args 1, args 2, args 3))
+      MConstr (Munify, (args 0, args 1, args 2, args 3, args 4, args 5, args 6))
   | Munify_univ ->
       MConstr (Munify_univ, (args 0, args 1, args 2))
   | Mget_reference ->
